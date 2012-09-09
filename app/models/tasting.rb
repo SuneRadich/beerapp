9a857e08fb4e1c55936b07a62a3f506cccbca0d3
Sune Radich Christensen <sykokaj@gmail.com>
2012-09-09 22:11:25 +0200

    Added relation between tasting and user

--- app/models/tasting.rb
+++ app/models/tasting.rb
@@ -1,4 +1,5 @@
 class Tasting < ActiveRecord::Base
   attr_accessible :beer_id, :clarityClarity, :clarityPrecipitate, :color, :coment, :date, :foamAmout, :foamColor, :foamDensity, :foamTime, :mouthCarbon, :mouthFill, :mouthTemp, :scentFruit, :scentHopsFlower, :scentMalt, :score, :tasteAftertaste, :tasteBitterness, :tasteSweetness, :user_id
   belongs_to :beer
+  belongs_to :user
 end
