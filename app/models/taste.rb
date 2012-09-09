daea440198cb3835ccd97e5269773734e819b1d5
Sune Radich Christensen <sykokaj@gmail.com>
2012-09-09 22:51:49 +0200

    Removing deleted files from git

--- app/models/taste.rb
+++
@@ -1,3 +0,0 @@
-class Taste < ActiveRecord::Base
-  attr_accessible :beer_id, :clarityClarity, :clarityPrecipitate, :color, :coment, :date, :foamAmout, :foamColor, :foamDensity, :foamTime, :mouthCarbon, :mouthFill, :mouthTemp, :scentFruit, :scentHopsFlower, :scentMalt, :score, :tasteAftertaste, :tasteBitterness, :tasteSweetness, :user_id
-end
--- test/unit/helpers/tastes_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class TastesHelperTest < ActionView::TestCase
-end
