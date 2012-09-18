945250a85d2998724f71cc1637c7bb05795b3523
Sune Radich Christensen <sykokaj@gmail.com>
2012-09-18 23:08:35 +0200

    Added tastings partial to show user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,9 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-
+    
+    @tastings = @user.tastings(page: params[:page])
+    
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
